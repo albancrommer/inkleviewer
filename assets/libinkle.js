3cd0e0fb307807aeb0077ffc6250a362b2cbdde9
Alban Crommer <alban@octopuce.fr>
2021-08-02 16:01:34 +0200

    [fix] It should work on old browsers (cont.)

--- assets/libinkle.js
+++ assets/libinkle.js
@@ -1,3 +1,4 @@
+'use strict';
 
 /**
  * Story Model
@@ -5,7 +6,7 @@
  * @param {string} string   Stringified JSON object
  * @class
  */
-var storyModel = function (string, inkle) {
+var storyModel = function storyModel(string, inkle) {
     this.inkle = inkle;
     this.story = JSON.parse(string);
     this.stitches = this.story.data.stitches;
@@ -16,7 +17,7 @@ var storyModel = function (string, inkle) {
         updatedAt: this.story.updated_at,
         title: this.story.title,
         initial: this.story.data.initial,
-        stitchesCount: _.size(this.stitches),
+        stitchesCount: _.size(this.stitches)
     };
     var stitches = this.story.data.stitches;
     var stitchesKeys = _.keys(this.story.data.stitches).sort();
@@ -42,55 +43,81 @@ storyModel.prototype.getStitch = function (stitch_name) {
  * @param {object} stitch A serialized stitch to be hydrated
  * @param {string} name the ID of the stitch
  */
-var stitchModel = function (stitch, name, inkle) {
+var stitchModel = function stitchModel(stitch, name, inkle) {
+    var _this = this;
+
     var flagList = inkle.flagList;
     var content = stitch.content;
     this.name = name || 'unknown';
     this.choices = {};
-    content.forEach((value, key) => {
+    var _a = content;
+
+    var _f = function _f(value, key) {
         // is it a message
         if (_.isString(value)) {
-            this.message = value;
+            _this.message = value;
         }
         // is it a divert
         else if (_.has(value, 'divert')) {
-            this.divert = value.divert;
-        }
-        // is it an image
-        else if (_.has(value, 'image')) {
-            this.image = value.image;
-        }
-        // is it a flag
-        else if (_.has(value, 'flagName')) {
-            flagList.push(value['flagName']);
-        }
-        // it it a choice
-        else if (_.has(value, 'linkPath')) {
-            // it should check conditions
-            var absentFlagErrors = [];
-            var presentFlagErrors = [];
-            debugger
-            // has negate conditions
-            if (value['notIfConditions'] && value['notIfConditions'].length > 0) {
-                value['notIfConditions'].forEach(function (flagContainer) {
-                    if (flagList.indexOf(flagContainer['notIfCondition']) !== -1) {
-                        absentFlagErrors.push(flagContainer['notIfCondition']);
+                _this.divert = value.divert;
+            }
+            // is it an image
+            else if (_.has(value, 'image')) {
+                    _this.image = value.image;
+                }
+                // is it a flag
+                else if (_.has(value, 'flagName')) {
+                        flagList.push(value['flagName']);
                     }
-                });
-            }
-            // has positive conditions
-            if (value['ifConditions'] && value['ifConditions'].length > 0) {
-                value['ifConditions'].forEach(function (flagContainer) {
-                    if (flagList.indexOf(flagContainer['ifCondition']) === -1) {
-                        presentFlagErrors.push(flagContainer['ifCondition']);
-                    }
-                });
-            }
-            if (absentFlagErrors.length === 0  && presentFlagErrors.length === 0 ) {
-                this.choices[value['linkPath']] = value['option'];
-            }
-        }
-    });
+                    // it it a choice
+                    else if (_.has(value, 'linkPath')) {
+                            // it should check conditions
+                            var absentFlagErrors = [];
+                            var presentFlagErrors = [];
+                            debugger;
+                            // has negate conditions
+                            if (value['notIfConditions'] && value['notIfConditions'].length > 0) {
+                                var _a2 = value['notIfConditions'];
+
+                                var _f2 = function _f2(flagContainer) {
+                                    if (flagList.indexOf(flagContainer['notIfCondition']) !== -1) {
+                                        absentFlagErrors.push(flagContainer['notIfCondition']);
+                                    }
+                                };
+
+                                for (var _i2 = 0; _i2 < _a2.length; _i2++) {
+                                    _f2(_a2[_i2], _i2, _a2);
+                                }
+
+                                undefined;
+                            }
+                            // has positive conditions
+                            if (value['ifConditions'] && value['ifConditions'].length > 0) {
+                                var _a3 = value['ifConditions'];
+
+                                var _f3 = function _f3(flagContainer) {
+                                    if (flagList.indexOf(flagContainer['ifCondition']) === -1) {
+                                        presentFlagErrors.push(flagContainer['ifCondition']);
+                                    }
+                                };
+
+                                for (var _i3 = 0; _i3 < _a3.length; _i3++) {
+                                    _f3(_a3[_i3], _i3, _a3);
+                                }
+
+                                undefined;
+                            }
+                            if (absentFlagErrors.length === 0 && presentFlagErrors.length === 0) {
+                                _this.choices[value['linkPath']] = value['option'];
+                            }
+                        }
+    };
+
+    for (var _i = 0; _i < _a.length; _i++) {
+        _f(_a[_i], _i, _a);
+    }
+
+    undefined;
     this.isFinal = function () {
         return content.length === 1;
     };
@@ -116,7 +143,7 @@ var stitchModel = function (stitch, name, inkle) {
  * @returns {nm$_libinkle.inkle}
  * @class
  */
-inkle = function (options) {
+var inkle = function inkle(options) {
     _.assign(this, options);
     this.flagList = [];
     this.paragraphList = null;
@@ -134,7 +161,7 @@ inkle = function (options) {
  * @returns {nm$_libinkle.storyModel|Object.prototype.parse.story}
  */
 inkle.prototype.parse = function (string) {
-    const story = new storyModel(string, this);
+    var story = new storyModel(string, this);
     return story;
 };
 
@@ -145,7 +172,7 @@ inkle.prototype.parse = function (string) {
  * @returns {inkle.prototype.stats.story.stats|.inkle.prototype@call;parse.stats|Object.prototype.stats.stats}
  */
 inkle.prototype.stats = function (story) {
-    var story = (typeof story === 'string') ? inkle.prototype.parse(story) : story;
+    var story = typeof story === 'string' ? inkle.prototype.parse(story) : story;
     var stats = story.stats;
     return stats;
 };
@@ -158,7 +185,7 @@ inkle.prototype.stats = function (story) {
  */
 inkle.prototype.start = function (stitch_name) {
     stitch_name = stitch_name || this.story.stats.initial;
-    const initial = this.story.getStitch(stitch_name);
+    var initial = this.story.getStitch(stitch_name);
     this.currentStitches = this.getAllStitches(initial);
     return this;
 };
@@ -184,7 +211,7 @@ inkle.prototype.getAllStitches = function (currentStitch) {
             final = true;
         } else {
             nextStitch = currentStitch.nextStitch();
-            currentStitch = this.story.getStitch(nextStitch)
+            currentStitch = this.story.getStitch(nextStitch);
         }
     }
     return true;
